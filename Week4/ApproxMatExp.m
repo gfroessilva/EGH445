d43a85c5039a4ac493c8e158d0e611eaac18900d
admcfadyen <79479523+admcfadyen@users.noreply.github.com>
2025-03-19 10:35:43 +1000

    Update ApproxMatExp.m - Include H matrix appx

--- Week4/ApproxMatExp.m
+++ Week4/ApproxMatExp.m
@@ -10,22 +10,23 @@
 
 %% Function Definition
 
-function [G] = ApproxMatExp(A, T, n, showsolution)
+function [G, H] = ApproxMatExp(A, B, T, n, showsolution)
 % A = Continuous time plant matrix (A)
+% A = Continuous time input matrix (B)
 % T = Sampling Time
 % n = Order of approximation for matrix exponential
 
 G = eye(size(A));
+H = zeros(size(B));
 
 for i=1:1:n
-    G = G + A^i.*T^i.*(1/factorial(i)); 
+    G = G + A^i.*T^i.*(1/factorial(i));
+    %------ STUDENTS: Insert code to approximate H ------
+    % H = ?
+    %----------------------------------------------------
     if showsolution
         disp(horzcat('Matrix Exponetial Approximation - Order ',num2str(i),':'))
-        G
+        disp('G: '); G
+        disp('H: '); H
     end
 end
-
-
-% Resources
-%G3 = eye(size(S.A)) + S.A.*T + S.A^2.*T^2.*(1/factorial(2)) + ...
-%    S.A^3.*T^3.*(1/factorial(3))
